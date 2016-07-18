88a7d35703b310ff67056740d9508e8968f95e5c
pdigiglio <p.digiglio91@gmail.com>
2016-07-18 12:13:39 +0200

    CallStack -> Stack<T>; typedef CallStack

--- include/CallStack.h
+++ include/CallStack.h
@@ -16,33 +16,36 @@
 
 #include <vector>
 
-enum class StackAction: bool {
-	PUSH = 0,
-	POP  = 1
-};
+//enum class StackAction: bool {
+//	PUSH = 0,
+//	POP  = 1
+//};
 
 /// Simple stack wrapping a `std::vector`
-class CallStack {
+template <class T>
+class Stack {
 	public:
 		/// Pop a value from the top of the stack
-		const CAL_ID pop() {
-			CAL_ID call = Stack_.back();
+		const T pop() {
+			T call = Stack_.back();
 			Stack_.pop_back();
 			return call;
 		}
 		/// Push a value on top of the stack
-		void push(CAL_ID id) { Stack_.push_back(id); }
+		void push(T id) { Stack_.push_back(id); }
 
 		/// Check whether the stack is empty
 		const bool isEmpty() const { return Stack_.empty(); }
 
 		/// Top of the stack
 		/// @return The value at the top of the stack without popping it
-		const CAL_ID& top() const { return Stack_.back(); }
+		const T& top() const { return Stack_.back(); }
 
 		
 	private:
-		std::vector<CAL_ID> Stack_;
+		std::vector<T> Stack_;
 };
+
+using CallStack = Stack<CAL_ID>;
 
 #endif
